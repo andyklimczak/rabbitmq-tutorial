611410eb3dc2f24b8d3f3169bb6d720a50c4f639
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-23 13:24:13 -0500

    hello world

---
+++ src/helloworld/send.js
@@ -0,0 +1,16 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+    var msg = 'Hello world';
+
+    ch.assertQueue(q, { durable: false });
+    ch.sendToQueue(q, new Buffer(msg));
+    console.log('[x] Sent %s', msg);
+  });
+  setTimeout(function() {
+    conn.close();
+    process.exit(0)
+  }, 500);
+});
